ad69b3418776f1c40c2fa953f7037d50a500ab5f
Guillaume Astier <astier.gui@gmail.com>
2024-07-04 21:47:12 +0200

    ci: update with screenshot

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,21 +1,31 @@
 module.exports = {
     src_folders: ["tests"],
-    output_folder: process.env.NIGHTWATCH_OUTPUT, // "var/nightwatch/output"
+    output_folder: `${process.env.NIGHTWATCH_OUTPUT}/reports`, // "var/nightwatch"
     webdriver: {
-        start_process: true,
-        server_path: "node_modules/.bin/chromedriver",
-        host: process.env.NIGHTWATCH_HOST, // "selenium",
-        port: process.env.NIGHTWATCH_PORT, // 4444,
-        ssl: false,
-        default_path_prefix: '',
-        proxy: undefined,
-        cli_args: {}
+        start_process: false,
     },
     test_settings: {
         default: {
+            selenium_port: process.env.WEBDRIVER_PORT || 4444,
+            selenium_host: process.env.WEBDRIVER_HOSTNAME || "localhost",
+            default_path_prefix: process.env.WEBDRIVER_PATH_PREFIX || '',
             desiredCapabilities: {
-                browserName: "chrome"
-            }
+                browserName: "chrome",
+                acceptSslCerts: true,
+                'goog:chromeOptions': {
+                     args: process.env.WEBDRIVER_CHROME_ARGS
+                         ? process.env.WEBDRIVER_CHROME_ARGS.split(' ')
+                         : [],
+                },
+            },
+            screenshots: {
+                enabled: true,
+                on_failure: true,
+                on_error: true,
+                path: `${process.env.NIGHTWATCH_OUTPUT}/screenshots`,
+            },
+            end_session_on_fail: false,
+            skip_testcases_on_fail: false,
         }
     }
 };
--- tests/ExampleTest.js
+++ tests/ExampleTest.js
@@ -1,10 +1,12 @@
 module.exports = {
     '@tags': ['custom'],
     'Demo test Google': function (browser) {
+        const screenshotPath = browser.options.screenshots.path;
         browser
             .url('http://google.com')
             .waitForElementVisible('body', 1000)
-            .assert.titleContains('Google');
+            .assert.titleContains('Google')
+            .saveScreenshot(`${screenshotPath}/homepage-google.png`);
     },
     after(browser) {
         browser.end();
